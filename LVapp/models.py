f55b48a2499b4e6f18af2c82749ae17ce7c5e278
evobuilder87 <95878498+evobuilder87@users.noreply.github.com>
2021-12-10 21:49:52 +0100

    Update models.py

--- LVapp/models.py
+++ LVapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class LVapp(models.Model):
-    immagine = models.ImageField()
     nome = models.CharField(max_length=20)
     prezzo = models.FloatField()
     
